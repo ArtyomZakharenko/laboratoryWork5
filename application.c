5e54f5abf26f11ba5f2ab18e5341a76345aa858a
ArtyomZakharenko <61537821+ArtyomZakharenko@users.noreply.github.com>
2020-10-21 22:58:53 +0300

    added "CreateArray" function

--- application.c
+++ application.c
@@ -1,1 +1,23 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int ** CreateArray (int *);
+
+int main()
+{
+
+    return 0;
+}
+
+int ** CreateArray (int * size){
+    int i;
+    printf("Enter dimenshion of array: ");
+    scanf("%d", size);
+    int **arr = (int **)calloc(*size, sizeof(int *));
+    for (i = 0; i < *size; i++){
+        arr[i] = (int*)calloc(*size, sizeof(int));
+    }
+    
+    return arr;
+}
