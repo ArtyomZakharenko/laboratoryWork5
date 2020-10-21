191f2b0a5a3bac220317fd829132cd5a86e59e35
ArtyomZakharenko <61537821+ArtyomZakharenko@users.noreply.github.com>
2020-10-22 00:56:29 +0300

    added "ShowArray" function

--- application.c
+++ application.c
@@ -2,18 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int ** CreateArray (int *);
-void FillArray (int**, int );
+int ** CreateArray (int *, int);
+void FillArray (int **, int);
+void ShowArray (int **, int, int);
 
 int main()
 {
+    int first = 1;
+    int second = 2;
+    int **firstArray = NULL;
+    int **secondArray = NULL;
+    int firstArrayDimension, secondArrayDimension;
+    firstArray = CreateArray(&firstArrayDimension, first);
+    FillArray(firstArray, firstArrayDimension);
+    ShowArray(firstArray, firstArrayDimension, first);
 
     return 0;
 }
 
-int ** CreateArray (int * size){
+int ** CreateArray (int * size, int number){
     int i;
-    printf("Enter dimenshion of array: ");
+    printf("Enter dimenshion of array %d: ", number);
     scanf("%d", size);
     int **arr = (int **)calloc(*size, sizeof(int *));
     for (i = 0; i < *size; i++){
@@ -30,6 +39,18 @@ void FillArray(int ** arr, int size){
         for(j = 0; j < size; j++){
             printf("row %d, element %d: ", i+1, j+1);
             scanf("%d", &arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void ShowArray (int ** arr, int size, int number){
+    int i, j;
+    printf("Your array %d:\n", number);
+    for (i = 0; i < size; i++){
+        for(j = 0; j < size; j++){
+            printf("%4d", arr[i][j]);
+            
         }
         printf("\n");
     }
